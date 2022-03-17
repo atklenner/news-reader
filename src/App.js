5ae1c8d8d032693503085555ea3e487e1e43c69c
Andrew <atklenner@gmail.com>
2022-03-17 16:02:52 -0700

    Connect API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { getStoryIds } from "./services/hnApi";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+
+  return <div>{JSON.stringify(storyIds)}</div>;
 }
-
-export default App;
---
+++ src/services/hnApi.js
@@ -0,0 +1,7 @@
+export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
+export const newStoriesUrl = `${baseUrl}newstories.json`;
+export const storyUrl = `${baseUrl}item/`;
+
+export const getStoryIds = () => {
+  return fetch(newStoriesUrl).then((res) => res.json());
+};
